2a23099e3457cc54590e47ec2260286067e3af89
“Robert <digitalmediaflow@gmail.com>
2020-05-20 04:26:16 -0400

    added form anf table state alrady added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './Components/Form';
+import TheTable from './Components/Table'
+function App() {
+  return (
+    <div className="App">
+     <Form/>
+     <TheTable/>
+    </div>
+  );
+}
+
+export default App;
